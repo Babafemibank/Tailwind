05f0934388e89349a19be78cbe9f4fd58c4d9222
Babafemibank <babafemiusman@gmail.com>
2025-11-10 16:23:43 +0100

    first-commit

---
+++ button-component/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-green-900 h-screen w-9xl p-24'>
+      <div className=' h-180 bg-white  border-8 border-green-500 p-4 rounded-lg'>
+         <div className=' h-150 bg-red-900  border-2 border-green-500 rounded-lg p-4 mx-auto my-auto'>
+          <div className=' h-120 bg-green-900  border-2 border-white rounded-lg'>
+            <p className='text-5xl text-white'>Hello Tailwind CSS</p>
+            <p className='text-3xl text-white'>Hello Tailwind CSS</p>
+            <p className='text-lg text-white'>Hello Tailwind CSS</p>
+            <p className='text-md text-white'>Hello Tailwind CSS</p>
+            <p className='text-sm text-white'>Hello Tailwind CSS</p>
+            <p className='text-xs text-white'>Hello Tailwind CSS</p>
+          </div>
+         </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ profile-card/src/App.jsx
@@ -0,0 +1,182 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-between items-center max-w-9xl mx-auto bg-gradient-to-r from-green-500 via-white to-green-500 h-32 p-2">
+        <div className="flex gap-2 items-end">
+          <div className="rounded-full w-28 h-28 bg-gray-400 border-4 border-white"></div>
+          <div>
+            <p className="text-red-500">John Doe</p>
+            <p className="text-red-800">AI Developer</p>
+            <p className="text-red-800">Kopabe, Abeokuta, Nigeria.</p>
+          </div>
+        </div>
+        <div className="flex gap-4">
+          <button className="bg-red-300 border-4 border-red-950 w-32 h-12 text-lg rounded-md shadow-lg">
+            followers
+          </button>
+          <button className="bg-red-300 border-4 border-red-950 w-32 h-12 text-lg rounded-md shadow-lg">
+            posts
+          </button>
+          <button className="bg-red-300 border-4 border-red-950 w-32 h-12 text-lg rounded-md shadow-lg">
+            likes
+          </button>
+            <button className="bg-blue-300 border-4 border-red-950 w-32 h-12 text-lg rounded-md shadow-lg">
+           follow
+          </button>
+        </div>
+      </div>
+      <div className="bg-red-50 p-4 h-auto">
+        <h2 className="text-center text-xl font-bold">Bio Section</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore
+          tenetur aliquam, facere, tempora ducimus maxime doloribus eum nemo
+          inventore veniam expedita unde esse soluta, similique rem optio
+          accusamus quibusdam repellendus.
+        </p>
+        <p>
+         
+          A, deserunt sapiente culpa molestiae impedit magni nam corrupti esse
+          voluptates ipsam quo inventore voluptatum in modi debitis id
+          perferendis distinctio neque. Alias nam ipsam exercitationem beatae
+          laborum aliquam possimus. Aspernatur, quo.
+        </p>
+        <p>
+          Adipisci blanditiis atque dicta quos labore, quae voluptatem nihil
+          quam explicabo, molestiae fugit mollitia. Quam facilis quis
+          accusantium expedita totam vitae voluptas ad, dignissimos temporibus
+          suscipit odit voluptatum! Inventore, minus. Magnam ipsa ratione
+          impedit quia quam cum omnis.
+        </p>
+         <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore
+          tenetur aliquam, facere, tempora ducimus maxime doloribus eum nemo
+          inventore veniam expedita unde esse soluta, similique rem optio
+          accusamus quibusdam repellendus.
+        </p>
+        <p>
+         
+          A, deserunt sapiente culpa molestiae impedit magni nam corrupti esse
+          voluptates ipsam quo inventore voluptatum in modi debitis id
+          perferendis distinctio neque. Alias nam ipsam exercitationem beatae
+          laborum aliquam possimus. Aspernatur, quo.
+        </p>
+        <p>
+          Adipisci blanditiis atque dicta quos labore, quae voluptatem nihil
+          quam explicabo, molestiae fugit mollitia. Quam facilis quis
+          accusantium expedita totam vitae voluptas ad, dignissimos temporibus
+          suscipit odit voluptatum! Inventore, minus. Magnam ipsa ratione
+          impedit quia quam cum omnis.
+        </p>
+         <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore
+          tenetur aliquam, facere, tempora ducimus maxime doloribus eum nemo
+          inventore veniam expedita unde esse soluta, similique rem optio
+          accusamus quibusdam repellendus.
+        </p>
+        <p>
+         
+          A, deserunt sapiente culpa molestiae impedit magni nam corrupti esse
+          voluptates ipsam quo inventore voluptatum in modi debitis id
+          perferendis distinctio neque. Alias nam ipsam exercitationem beatae
+          laborum aliquam possimus. Aspernatur, quo.
+        </p>
+        <p>
+          Adipisci blanditiis atque dicta quos labore, quae voluptatem nihil
+          quam explicabo, molestiae fugit mollitia. Quam facilis quis
+          accusantium expedita totam vitae voluptas ad, dignissimos temporibus
+          suscipit odit voluptatum! Inventore, minus. Magnam ipsa ratione
+          impedit quia quam cum omnis.
+        </p>
+         <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore
+          tenetur aliquam, facere, tempora ducimus maxime doloribus eum nemo
+          inventore veniam expedita unde esse soluta, similique rem optio
+          accusamus quibusdam repellendus.
+        </p>
+        <p>
+         
+          A, deserunt sapiente culpa molestiae impedit magni nam corrupti esse
+          voluptates ipsam quo inventore voluptatum in modi debitis id
+          perferendis distinctio neque. Alias nam ipsam exercitationem beatae
+          laborum aliquam possimus. Aspernatur, quo.
+        </p>
+        <p>
+          Adipisci blanditiis atque dicta quos labore, quae voluptatem nihil
+          quam explicabo, molestiae fugit mollitia. Quam facilis quis
+          accusantium expedita totam vitae voluptas ad, dignissimos temporibus
+          suscipit odit voluptatum! Inventore, minus. Magnam ipsa ratione
+          impedit quia quam cum omnis.
+        </p>
+          <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore
+          tenetur aliquam, facere, tempora ducimus maxime doloribus eum nemo
+          inventore veniam expedita unde esse soluta, similique rem optio
+          accusamus quibusdam repellendus.
+        </p>
+        <p>
+         
+          A, deserunt sapiente culpa molestiae impedit magni nam corrupti esse
+          voluptates ipsam quo inventore voluptatum in modi debitis id
+          perferendis distinctio neque. Alias nam ipsam exercitationem beatae
+          laborum aliquam possimus. Aspernatur, quo.
+        </p>
+        <p>
+          Adipisci blanditiis atque dicta quos labore, quae voluptatem nihil
+          quam explicabo, molestiae fugit mollitia. Quam facilis quis
+          accusantium expedita totam vitae voluptas ad, dignissimos temporibus
+          suscipit odit voluptatum! Inventore, minus. Magnam ipsa ratione
+          impedit quia quam cum omnis.
+        </p>
+          <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore
+          tenetur aliquam, facere, tempora ducimus maxime doloribus eum nemo
+          inventore veniam expedita unde esse soluta, similique rem optio
+          accusamus quibusdam repellendus.
+        </p>
+        <p>
+         
+          A, deserunt sapiente culpa molestiae impedit magni nam corrupti esse
+          voluptates ipsam quo inventore voluptatum in modi debitis id
+          perferendis distinctio neque. Alias nam ipsam exercitationem beatae
+          laborum aliquam possimus. Aspernatur, quo.
+        </p>
+        <p>
+          Adipisci blanditiis atque dicta quos labore, quae voluptatem nihil
+          quam explicabo, molestiae fugit mollitia. Quam facilis quis
+          accusantium expedita totam vitae voluptas ad, dignissimos temporibus
+          suscipit odit voluptatum! Inventore, minus. Magnam ipsa ratione
+          impedit quia quam cum omnis.
+        </p>
+          <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore
+          tenetur aliquam, facere, tempora ducimus maxime doloribus eum nemo
+          inventore veniam expedita unde esse soluta, similique rem optio
+          accusamus quibusdam repellendus.
+        </p>
+        <p>
+         
+          A, deserunt sapiente culpa molestiae impedit magni nam corrupti esse
+          voluptates ipsam quo inventore voluptatum in modi debitis id
+          perferendis distinctio neque. Alias nam ipsam exercitationem beatae
+          laborum aliquam possimus. Aspernatur, quo.
+        </p>
+        <p>
+          Adipisci blanditiis atque dicta quos labore, quae voluptatem nihil
+          quam explicabo, molestiae fugit mollitia. Quam facilis quis
+          accusantium expedita totam vitae voluptas ad, dignissimos temporibus
+          suscipit odit voluptatum! Inventore, minus. Magnam ipsa ratione
+          impedit quia quam cum omnis.
+        </p>
+        <p>
+        Accusamus quos iure doloribus est voluptatum adipisci? Minima, officiis,
+        omnis laboriosam reprehenderit rerum nemo quisquam aliquam praesentium
+        cum totam adipisci. Cumque sunt facilis recusandae, dolore minima
+        distinctio voluptas rerum quibusdam harum ea eligendi ratione quam eum
+        quasi aperiam animi impedit ullam cupiditate quaerat sapiente veritatis
+        inventore, in sit! Nesciunt, deleniti?</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
